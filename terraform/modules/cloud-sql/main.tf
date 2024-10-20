1a51043c7aaf2b2101373c90c3a21d087e4a7aa8
Jace Romero <39967529+Romero-jace@users.noreply.github.com>
2024-10-19 21:08:08 -0500

    save updates

---
+++ terraform/modules/cloud-sql/main.tf
@@ -0,0 +1,53 @@
+resource "google_sql_database_instance" "default" {
+  name             = var.instance_name
+  database_version = "MYSQL_8_4"
+  region           = var.region
+  project          = var.project_id
+
+  settings {
+    tier              = "db-custom-1-1792"
+    availability_type = "ZONAL"
+
+    backup_configuration {
+      enabled    = true
+      start_time = var.backup_start_time
+    }
+
+    ip_configuration {
+      ipv4_enabled = true
+    }
+
+    disk_autoresize = true
+    disk_size       = 10
+    disk_type       = "PD_STANDARD"
+
+    location_preference {
+      zone = "us-central1-c"
+    }
+
+    maintenance_window {
+      day          = 7
+      hour         = 3
+      update_track = "stable"
+    }
+  }
+
+  deletion_protection = false
+}
+
+resource "google_sql_database" "default" {
+
+  name      = var.database_name
+  instance  = google_sql_database_instance.default.name
+  project   = var.project_id
+  charset   = "utf8"
+  collation = "utf8_general_ci"
+}
+
+resource "google_sql_user" "default" {
+  name     = var.db_user_name
+  instance = google_sql_database_instance.default.name
+  project  = var.project_id
+  password = var.db_password
+}
+
--- terraform/variables.tf
+++ terraform/variables.tf
@@ -1,1 +1,10 @@
+variable "project_id" {
+  type        = string
+  description = "The ID of the Google Cloud project."
+  default     = "resume-portfolio-project"
+}
 
+variable "region" {
+  description = "The region to deploy the Cloud SQL instance"
+  type        = string
+}
