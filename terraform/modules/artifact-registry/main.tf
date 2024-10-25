d71ebdba79831f9dfee4ac535357bc77376ea49e
Jace Romero <39967529+Romero-jace@users.noreply.github.com>
2024-10-25 10:38:31 -0500

    Update main.tf

--- terraform/modules/artifact-registry/main.tf
+++ terraform/modules/artifact-registry/main.tf
@@ -11,7 +11,7 @@ resource "google_artifact_registry_repository" "iac_repo" {
   location      = var.region
   repository_id = var.repo_name
   format        = "DOCKER"
-  description   = "Resume Portfolio Docker Images and Helm Charts"
+  description   = "Resume Portfolio Docker Images"
 
   docker_config {
     immutable_tags = false
