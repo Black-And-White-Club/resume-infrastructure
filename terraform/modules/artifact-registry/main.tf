83d35efdc4b066f536c9f65bd20babd6602988ed
Jace Romero <39967529+Romero-jace@users.noreply.github.com>
2024-10-24 20:07:42 -0500

    updates have been made
    modifying artifact registry just to be clean for helm charts

--- terraform/modules/artifact-registry/main.tf
+++ terraform/modules/artifact-registry/main.tf
@@ -6,12 +6,12 @@ terraform {
     }
   }
 }
-resource "google_artifact_registry_repository" "docker_repo" {
-  project       = var.gcp_project_id
+resource "google_artifact_registry_repository" "iac_repo" {
+  project       = var.project_id
   location      = var.region
   repository_id = var.repo_name
   format        = "DOCKER"
-  description   = "Resume Portfolio Docker Images"
+  description   = "Resume Portfolio Docker Images and Helm Charts"
 
   docker_config {
     immutable_tags = false
--- terraform/modules/artifact-registry/variables.tf
+++ terraform/modules/artifact-registry/variables.tf
@@ -1,4 +1,4 @@
-variable "gcp_project_id" {
+variable "project_id" {
   description = "GCP Project ID"
   type        = string
 }
@@ -6,11 +6,9 @@ variable "gcp_project_id" {
 variable "region" {
   description = "GCP region where the Artifact Registry will be created"
   type        = string
-  default     = "us-central1"
 }
 
 variable "repo_name" {
   description = "The name of the Artifact Registry repository"
   type        = string
-  default     = "portfolio-app-images"
 }
